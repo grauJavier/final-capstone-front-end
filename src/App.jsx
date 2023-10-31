cedc31ae6a68804687b766f24b3dfea4922304b1
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-31 10:05:24 -0500

    Add the register component in the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/home'
 import NotFound from './components/NotFound'
+import Register from './components/Register'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
