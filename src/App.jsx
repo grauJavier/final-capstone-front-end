51fdf03c298a4a47aa2742dd3265ee78b1268ac7
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-31 17:49:42 -0500

    Add the login path for the Login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/home'
 import NotFound from './components/NotFound'
 import Register from './components/Register_Login_forms/Register'
+import Login from './components/Register_Login_forms/Login'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
