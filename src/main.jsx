e2f173451b04954121f958b4d217d3656f813766
lRebornsl <anthony_vasquez98@hotmail.com>
2023-11-03 16:27:34 -0500

    Fix linters

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './redux/store.js'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
