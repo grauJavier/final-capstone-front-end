a242e590cbdcaf94fbedccb593dc0ee3513689ac
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-03 12:51:38 -0500

    Added a close icon to close the menu and show the hamburguer icon

--- src/components/Navigation_bar/NavBar.jsx
+++ src/components/Navigation_bar/NavBar.jsx
@@ -1,23 +1,28 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiFillCloseCircle } from 'react-icons/ai';
 import { useState } from 'react';
 import Logout from '../Register_Login_forms/Logout';
 
 const NavBar = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
+  const [showItem, setShowItem] = useState(true);
 
-  const handleClick = () => {
+  const showHideNav = () => {
     if (show) {
       setShow(false);
+      setShowItem(true);
     } else {
       setShow(true);
+      setShowItem(false);
     }
   };
 
   return (
     <>
-      <GiHamburgerMenu onClick={handleClick} />
+      {showItem && <GiHamburgerMenu onClick={showHideNav} />}
       { show && 
       <div>
+        <AiFillCloseCircle onClick={showHideNav}/>
         <img src="src/assets/logo-black.png" className="w-[100px]" />
         <nav>
           <ul className="flex flex-col">
