20875f34792e9912eec7458e87e1a1d7ad139b3f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-03 16:52:12 -0500

    Show and hide elements depending on the window's width

--- src/components/Navigation_bar/NavBar.jsx
+++ src/components/Navigation_bar/NavBar.jsx
@@ -1,12 +1,25 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiFillCloseCircle } from 'react-icons/ai';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Logout from '../Register_Login_forms/Logout';
 
 const NavBar = () => {
   const [show, setShow] = useState(false);
   const [showItem, setShowItem] = useState(true);
   const path = window.location.pathname;
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setwindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const showHideNav = () => {
     if (show) {
@@ -18,26 +31,15 @@ const NavBar = () => {
     }
   };
 
-  /* const handleTap = (event) => {
-    if (showItem) return;
-    const container = document.getElementById('menu-container');
-    if (event.target !== container) {
-      setShow(false);
-      setShowItem(true);
-    }
-  };
-
-  document.addEventListener('click', handleTap); */
-
   const elements = [['Places', '/'], ['My Reservations', '/my-reservations'], ['New Reservation', '/new-reservation'], ['New Place', '/new-place'], ['Remove Place', '/remove-place']];
 
   return (
     <>
-      {showItem && <GiHamburgerMenu className="fixed ml-[15px] mt-[15px] text-[1.5rem]" onClick={showHideNav} />}
-      { show && 
+      {showItem && windowWidth < 1024 && <GiHamburgerMenu className="fixed ml-[15px] mt-[15px] text-[1.5rem]" onClick={showHideNav} />}
+      { (show || windowWidth >= 1024) && 
       <div className="min-h-screen w-3/4 max-w-[320px] flex flex-col primary-font fixed z-50 bg-white shadow-menu" id="menu-container">
-        <AiFillCloseCircle className="ml-[15px] mt-[15px] text-[1.5rem]" onClick={showHideNav}/>
-        <img src="src/assets/logo-black.png" className="w-[100px] ml-auto mr-auto" />
+        <AiFillCloseCircle className="ml-[15px] mt-[15px] text-[1.5rem] lg:hidden" onClick={showHideNav}/>
+        <img src="src/assets/logo-black.png" className="w-[100px] ml-auto mr-auto lg:mt-auto" />
         <nav className="m-auto">
           <ul className="flex flex-col content-center min-h-[70vh] justify-evenly">
             {elements.map((element, index) => 
