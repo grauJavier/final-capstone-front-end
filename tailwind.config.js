b5c5c66d264c699eb0314182ff45c4d1c4dcc25d
lRebornsl <anthony_vasquez98@hotmail.com>
2023-11-03 16:04:12 -0500

    Modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'limerick': '#98BF11',
+        'platinum': '#E4E5E9',
+        'argent': '#C1C1C1',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'titillium': ['Titillium Web', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
