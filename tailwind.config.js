aefb840e0511ff3b691620ba06952e1ecd2968f2
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-04 11:22:57 -0300

    Add color pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lime-green': '#f1f1f1',
+        'golden-yellow': '#ffc600',
+        'turquoise-blue': '#10bbb5',
+        'charcoal-gray': '#252525',
+        'platinum': '#e4e5e9',
+      },
+    },
   },
   plugins: [],
-}
+};
