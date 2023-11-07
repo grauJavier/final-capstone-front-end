4f0473c9df4d7f3d3a5062af981564c278aa2394
lRebornsl <anthony_vasquez98@hotmail.com>
2023-11-07 18:52:25 -0500

    Change title page

--- src/components/myPlaces/MyPlaces.jsx
+++ src/components/myPlaces/MyPlaces.jsx
@@ -3,7 +3,7 @@ import Slideshowid from '../subcomponents/SliderById';
 const MyPlaces = () => (
   <section className="max-w-full">
     <div className="flex flex-col justify-center text-center h-screen lg:ml-[15%] max-w-full">
-      <h1 className="uppercase font-black text-5xl tracking-wider font-poppins">Places</h1>
+      <h1 className="uppercase font-black text-5xl tracking-wider font-poppins">My Places</h1>
       <h2 className="text-argent font-bold text-md tracking-wide">
         Select a place to see more details
       </h2>
