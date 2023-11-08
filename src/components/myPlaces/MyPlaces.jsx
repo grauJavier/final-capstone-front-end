b189c8f3d468622d306590074440c3b47eb4e37f
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-08 13:06:58 -0300

    Add top margin to MyPlaces component

--- src/components/myPlaces/MyPlaces.jsx
+++ src/components/myPlaces/MyPlaces.jsx
@@ -2,7 +2,7 @@ import Slideshowid from '../subcomponents/SliderById';
 
 const MyPlaces = () => (
   <section className="max-w-full py-4 pt-20 md:p-0">
-    <div className="flex flex-col justify-center text-center h-screen lg:ml-[15%] max-w-full">
+    <div className="flex flex-col justify-center text-center h-screen lg:ml-[15%] max-w-full mt-8">
       <h1 className="uppercase font-black text-5xl tracking-wider font-poppins">My Places</h1>
       <h2 className="text-argent font-bold text-md tracking-wide">
         Select a place to see more details
