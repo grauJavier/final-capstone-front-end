9d7ffa008a66e6fec452434aacf4b0546d04da75
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-08 01:06:30 -0300

    Update delete button according to style

--- src/components/subcomponents/SliderById.jsx
+++ src/components/subcomponents/SliderById.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getPlacesById } from '../../redux/places/placesSlice';
 import { useNavigate } from 'react-router-dom';
 import { selectUserId } from '../../redux/user/userSlice.js';
-import { deletePlace } from "../../redux/places/placesSlice";
+import { deletePlace } from '../../redux/places/placesSlice';
 
 const Slideshowid = () => {
   const dispatch = useDispatch();
@@ -65,12 +65,8 @@ const Slideshowid = () => {
     <div>
       <Slider className="md:mx-24 mx-16 h-full" {...settings}>
         {placesById.map((place, index) => (
-          <div
-            className="cursor-pointer flex flex-col justify-center"
-            key={index}
-          >
-            <div className="flex justify-center"
-            onClick={() => goToPlaceDetails(place.id)}>
+          <div className="cursor-pointer flex flex-col justify-center" key={index}>
+            <div className="flex justify-center" onClick={() => goToPlaceDetails(place.id)}>
               <div
                 className="w-full sm:max-w-[17vw] max-w-[39vw] bg-cover bg-center aspect-square rounded-full hover:saturate-0 hover:opacity-40 transition-opacity ease-linear duration-[50ms]"
                 style={{ backgroundImage: `url(${place.image_url})` }}
@@ -89,12 +85,30 @@ const Slideshowid = () => {
               </div>
             </div>
             <div>
-              <button className='bg-golden-yellow w-20 rounded-xl mt-2' onClick={() => handleDelete(userId, place.id)}>Delete</button>
+              <button
+                className="flex items-center justify-center gap-2 mx-auto px-5 py-3 mt-4 rounded-full bg-red-500 text-white hover:bg-white hover:text-red-500 hover:border-red-500 border-2 border-red-500 transition ease-linear duration-200"
+                onClick={() => handleDelete(userId, place.id)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+                <span>Remove</span>
+              </button>
             </div>
           </div>
         ))}
       </Slider>
-
     </div>
   );
 };
