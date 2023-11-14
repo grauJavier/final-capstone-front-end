73f7c268195f3c3afb745c7fdecc59854b233098
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-14 20:44:23 -0300

    Add history API fallback to server config.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Enable history API fallback
+    historyApiFallback: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-  }
-})
+  },
+});
