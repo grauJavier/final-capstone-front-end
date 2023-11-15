db23d0a50bb1e1af5c18265104e41f9e17f82bf5
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-15 04:24:45 -0300

    Refactor tests and fix snapshot mismatch

--- src/__tests__/MyReservations.test.jsx
+++ src/__tests__/MyReservations.test.jsx
@@ -1,58 +1,68 @@
-import { render, waitFor } from '@testing-library/react';
-import MyReservations from '../components/my_reservations/MyReservations'; // Import the MyReservations component.
+import { render, screen } from '@testing-library/react';
+import MyReservations from '../components/my_reservations/MyReservations';
 import { describe, it, expect } from 'vitest';
 import { Provider } from 'react-redux';
-import { store } from '../redux/Store';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { store } from '../redux/store';
+
+const mockStore = configureStore([thunk]);
 
 describe('My Reservations Component', () => {
-  it('MyReservations renders correctly', async () => {
+  it('MyReservations renders correctly', () => {
     render(
       <Provider store={store}>
         <MyReservations />
       </Provider>
     );
-  
-    await waitFor(() => {
-      expect(MyReservations).toMatchSnapshot();
-    });
+
+    expect(MyReservations).toMatchSnapshot();
   });
 
-  it('Should show "You haven\'t registered any visit yet" when there are no reservations', async () => {
-    const { findByText } = render(
+  it('Should show "You haven\'t registered any visit yet" when there are no reservations', () => {
+    render(
       <Provider store={store}>
         <MyReservations />
       </Provider>
     );
 
-    await waitFor(() => {
-      expect(findByText("You haven't registered any visit yet. Be brave. Make one.")).toBeTruthy();
-    });
+    expect(screen.getByText(/You haven't registered any visit yet/)).toBeTruthy();
   });
 
-  it('Should display reservations when there are reservations', async () => {
-    const mockReservations = [
-      {
-        id: 1,
-        place: {
-          name: 'Sample Place 1',
-          city: {
-            name: 'Sample City 1',
-          },
-          image_url: 'sample-image-1.jpg',
-        },
-        schedule_date: new Date().toISOString(),
+  it('Should display reservations when there are reservations', () => {
+    const fedStore = mockStore({
+      user: {
+        currentUser: { id: 1, email: 'graujavier@gmail.com', username: 'Javier' },
       },
-    ];
+      myReservations: {
+        reservations: [
+          {
+            id: 1,
+            user_id: 1,
+            place_id: 1,
+            schedule_date: '2023-11-22',
+            created_at: '2023-11-08T23:28:39.425Z',
+            updated_at: '2023-11-08T23:28:39.425Z',
+            place: {
+              name: 'Test Hotel',
+              image_url: 'https://www.testimage.com/image.jpg',
+              description: 'Description of Beautiful Hotel',
+              city: {
+                name: 'Test City, Test Country',
+              },
+            },
+          },
+        ],
+      },
+    });
 
-    const { findByText, findByTestId } = render(
-      <Provider store={store}>
+    render(
+      <Provider store={fedStore}>
         <MyReservations />
       </Provider>
     );
 
-    await waitFor(() => {
-      expect(findByText("Sample Place 1")).toBeTruthy();
-      expect(findByText("Sample City 1")).toBeTruthy();
-    });
+      expect(screen.getByText('Test Hotel')).toBeTruthy();
+      expect(screen.getByText('Test City, Test Country')).toBeTruthy();
   });
 });
--- src/__tests__/PlaceDetails.test.jsx
+++ src/__tests__/PlaceDetails.test.jsx
@@ -1,105 +1,60 @@
-import { createMemoryHistory } from 'history';
-import { render, waitFor, fireEvent, screen, getByText } from '@testing-library/react';
-import PlaceDetails from '../components/place_details/PlaceDetails';
-import { describe, it, expect } from 'vitest';
-import { MemoryRouter, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from '../redux/Store';
-import { getDetails, sendPlaces } from '../redux/places/placesSlice';
+import configureStore from 'redux-mock-store';
+import PlaceDetails from '../components/place_details/PlaceDetails';
+import { MemoryRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import { createMemoryHistory } from 'history';
+import { describe, it, expect } from 'vitest';
 
-const sampleData = {
-  body: {
-    id: 1,
-    place_id: 1,
-    place_type: 'Entire Place',
-    bedrooms: 4,
-    beds: 9,
-    bathrooms: 1,
-    property_type: 'Guest House',
-    price: 1725,
-    created_at: '2023-11-08T17:33:32.063Z',
-    updated_at: '2023-11-08T17:33:32.063Z',
-    place: {
-      user_id: 3,
-      name: 'Magnificent Hotel',
-      image_url:
-        'https://images.unsplash.com/photo-1634072319894-107e61606191?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MjM3MzR8MHwxfHNlYXJjaHw1fHxob3RlbC1yb29tfGVufDB8Mnx8fDE2OTk0NTgyNDZ8MA&ixlib=rb-4.0.3&q=80&w=1080',
-      description: 'Description of Magnificent Hotel',
-      city: {
-        id: 1,
-        name: 'New York City, USA',
+const mockStore = configureStore([thunk]);
+const store = mockStore({
+  user: {
+    currentUser: { id: 1, email: 'graujavier@gmail.com', username: 'Javier' },
+  },
+  placesSlice: {
+    details: {
+      id: 1,
+      place_id: 1,
+      place_type: 'Entire Place',
+      bedrooms: 4,
+      beds: 9,
+      bathrooms: 1,
+      property_type: 'Guest House',
+      price: 1725,
+      created_at: '2023-11-08T17:33:32.063Z',
+      updated_at: '2023-11-08T17:33:32.063Z',
+      place: {
+        user_id: 3,
+        name: 'Magnificent Hotel',
+        image_url:
+          'https://images.unsplash.com/photo-1634072319894-107e61606191?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MjM3MzR8MHwxfHNlYXJjaHw1fHxob3RlbC1yb29tfGVufDB8Mnx8fDE2OTk0NTgyNDZ8MA&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Description of Magnificent Hotel',
+        city: {
+          id: 1,
+          name: 'New York City, USA',
+        },
       },
     },
   },
-};
+});
 
 describe('PlaceDetails Component', () => {
-  it('PlaceDetails renders correctly', async () => {
-    await store.dispatch(sendPlaces({ user_id: sampleData.body.place.user_id, body: sampleData }));
-    await store.dispatch(getDetails(sampleData.id));
-    await waitFor(() => {
-      render(
+  it('PlaceDetails renders correctly', () => {
+    render(
+      <Provider store={store}>
         <MemoryRouter>
-          <Provider store={store}>
-            <PlaceDetails />
-          </Provider>
+          <PlaceDetails />;
         </MemoryRouter>
-      );
-      console.log(screen.debug());
-      expect(PlaceDetails).toMatchSnapshot();
-    });
+      </Provider>
+    );
+
+    expect(PlaceDetails).toMatchSnapshot();
   });
 
-  it('Clicking "Return" link redirects to "/places"', async () => {
-    const history = createMemoryHistory({ initialEntries: ['/places'] });
+  it('Clicking "Return" link redirects to the previous page', async () => {
+    const history = createMemoryHistory({ initialEntries: ['/places', '/places/1/details'] });
 
-    await store.dispatch(getDetails(sampleData.id));
-    await waitFor(() => {
-      render(
-        <MemoryRouter history={history}>
-          <Provider store={store}>
-            <PlaceDetails />
-          </Provider>
-        </MemoryRouter>
-      );
-    });
-
-    const returnLink = screen.getByText(/Return/);
-    fireEvent.click(returnLink);
-
-    await waitFor(() => {
-      const currentURL = history.location.pathname;
-      expect(currentURL === '/places').toBe(true);
-    });
-  });
-
-  it('Clicking "Return" link redirects to "/my-places"', async () => {
-    const history = createMemoryHistory({ initialEntries: ['/my-places'] });
-
-    await store.dispatch(getDetails(sampleData.id));
-    await waitFor(() => {
-      render(
-        <MemoryRouter history={history}>
-          <Provider store={store}>
-            <PlaceDetails />
-          </Provider>
-        </MemoryRouter>
-      );
-    });
-
-    const returnLink = screen.getByText(/Return/);
-    fireEvent.click(returnLink);
-
-    await waitFor(() => {
-      const currentURL = history.location.pathname;
-      expect(currentURL === '/my-places').toBe(true);
-    });
-  });
-
-  it('Clicking "Make a visit reservation here!" link redirects to "/reservation"', async () => {
-    const history = createMemoryHistory({
-      initialEntries: [`/places/${sampleData.place_id}/details`],
-    });
     render(
       <Provider store={store}>
         <MemoryRouter history={history}>
@@ -108,11 +63,76 @@ describe('PlaceDetails Component', () => {
       </Provider>
     );
 
-    const reservationLink = screen.getByRole('link', { name: /Make a visit reservation here!/i });
-    fireEvent.click(reservationLink);
+    const returnLink = screen.getByText(/Return/);
+    fireEvent.click(returnLink);
 
-    const currentURL = history.location.pathname;
-    const expectedURL = `/reservation?city_id=${sampleData.place.city.id}&place_id=${sampleData.place_id}`;
-    expect(currentURL).toBe(expectedURL);
+    // There is no way to test the useNavigate hook, so we are simulating with history
+    history.back();
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/places');
+    });
+  });
+
+  it('"Make a visit reservation" has build the right href', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <PlaceDetails />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const reservationLink = screen.getByText(/Make a visit reservation here!/);
+    expect(reservationLink).toBeTruthy();
+
+    const expectedHref = '/reservation?city_id=1&place_id=1';
+    expect(reservationLink.getAttribute('href')).toBe(expectedHref);
+  });
+
+  it('"Make a visit reservation" has build the right href', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <PlaceDetails />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const reservationLink = screen.getByText(/Make a visit reservation here!/);
+    expect(reservationLink).toBeTruthy();
+
+    const expectedHref = '/reservation?city_id=1&place_id=1';
+    expect(reservationLink.getAttribute('href')).toBe(expectedHref);
+  });
+
+  const modifiedStore = mockStore({
+    ...store.getState(),
+    placesSlice: {
+      ...store.getState().placesSlice,
+      details: {
+        ...store.getState().placesSlice.details,
+        place: {
+          ...store.getState().placesSlice.details.place,
+          user_id: 1,
+        },
+      },
+    },
+  });
+
+  it('"Remove" button available only when the currentUser was the creator of the Place', () => {
+    render(
+      <Provider store={modifiedStore}>
+        <MemoryRouter>
+          <PlaceDetails />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const reservationLink = screen.queryByText(/Make a visit reservation here!/);
+    expect(reservationLink).toBeNull();
+
+    const removeButton = screen.getByText(/Remove this place/);
+    expect(removeButton).toBeTruthy();
   });
 });
