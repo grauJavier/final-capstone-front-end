054c8c32b6193bfd2b8451aff48581f1edd64310
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-14 21:09:04 -0300

    Revert "Add vite-plugin-ssr/plugin for server-side"
    This reverts commit 563db612fdb0130afd915477394508a0ecee13f9.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
