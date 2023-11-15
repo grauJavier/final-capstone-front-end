d10c5301bc3a59055209d3dc3a017b654f69d7b8
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-14 21:10:20 -0300

    Revert "Add Vite plugin for server-side rendering"
    This reverts commit 007c7dfefc4faa8a1a5e454f7b65a6db28d0334d.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ssr()],
+  plugins: [react()],
   server: {
     // Enable history API fallback
     historyApiFallback: true,
